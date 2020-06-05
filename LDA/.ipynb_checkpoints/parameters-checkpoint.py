7b543da0085292439c103acd6a675a67a3309ad0
nafisasghari <nafiseh1365@gmail.com>
2020-06-05 10:55:29 -0400

    update results

---
+++ LDA/.ipynb_checkpoints/parameters-checkpoint.py
@@ -0,0 +1,14 @@
+IN_DIR = 'data'
+OUT_DIR = 'out'
+FILENAME = 'MyA_TY2018_all.csv'
+
+TEXT_COL = "Please tell us why you were dissatisfied with today's experience. Please be as specific as possible."
+RENAMED_TEXT_COL = "dissatisfaction_reason"
+
+LANGUAGE = 'EN' #'FR' , 'ALL'
+
+
+####- NLP PREPROCESSING PARAMETERS- ####
+EXTRA_STOP_WORDS = []
+
+NUM_TOPICS = 9
