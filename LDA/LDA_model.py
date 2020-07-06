c6787ce3cdd63935042ce24aef7830e90ddedafa
nafisasghari <nafiseh1365@gmail.com>
2020-07-06 14:41:08 -0400

    added NMF

--- LDA/LDA_model.py
+++ LDA/LDA_model.py
@@ -20,6 +20,7 @@ from bokeh.models import Label
 from bokeh.io import output_notebook
 # from bokeh.io import export_png
 from tqdm import tqdm
+from gensim.models.nmf import Nmf
 
 # pyLDAVis
 import pyLDAvis.gensim
@@ -34,59 +35,84 @@ from NLP_preprocessing import *
 
 
 ## LDA function ###
-def lda(num_topics, corpus, dictionary):
+def topic_modeling(method, num_topics, corpus, dictionary):
+    
+    if method == 'LDA':
     # Build LDA model
-    model = gensim.models.ldamodel.LdaModel(corpus=corpus,
-                                            id2word=dictionary,
-                                            num_topics=num_topics,
-                                            random_state=42,
-                                            update_every=1,
-                                            chunksize=10,
-                                            passes=10,
-                                            alpha='symmetric',
-                                            iterations=100,
-                                            per_word_topics=True)
+        model = gensim.models.ldamodel.LdaModel(corpus=corpus,
+                                                id2word=dictionary,
+                                                num_topics=num_topics,
+                                                random_state=42,
+                                                update_every=1,
+                                                chunksize=10,
+                                                passes=10,
+                                                alpha='symmetric',
+                                                iterations=100,
+                                                per_word_topics=True)
+        
+    elif method == 'NMF':
+        #Build NMF model
+        model = Nmf(corpus=corpus,
+                                      num_topics=num_topics,
+                                      id2word=dictionary,
+                                      chunksize=10, 
+                                      passes=10,
+                                      kappa=0.5,
+                                      w_max_iter=200,
+                                      h_max_iter=50,
+                                      eval_every=1, 
+                                      normalize=True, 
+                                      random_state=42)
+    
+        
+        
+    else:
+        raise ValueError ('method is invalid')
+        
 
     return model
 
 
+
+
+
 # coherence vs number of topics plot -- run it once after preprocessing to choose the proper number of topics
-def coherence_vs_topics(texts, dictionary, corpus, min_number_topics=5, max_number_topics=15):
+def coherence_vs_topics(method, texts, dictionary, corpus, min_number_topics=5, max_number_topics=15):
     scores = []
     topics_range = tqdm(range(min_number_topics, max_number_topics + 1))
     for n_topic in topics_range:
-        ldamodel = lda(num_topics=n_topic, corpus=corpus, dictionary=dictionary)
+        model = topic_modeling(method = method, num_topics=n_topic, corpus=corpus, dictionary=dictionary)
         # Compute Coherence Score
-        coherence_model_lda = CoherenceModel(model=ldamodel, texts=texts, dictionary=dictionary, coherence='c_v')
+        coherence_model_lda = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
         coherence_lda = coherence_model_lda.get_coherence()
         scores.append(coherence_lda)
     
     trange = range(min_number_topics, max_number_topics + 1)
-    pd.DataFrame({'Number of Topics':trange, 'Coherence Score': scores}).to_csv(os.path.join(OUT_DIR, 'coherence_Topics_score.csv'))
+    pd.DataFrame({'Number of Topics':trange, 'Coherence Score': scores}).to_csv(os.path.join(OUT_DIR, f'coherence_Topics_score_{method}.csv'))
     plt.figure(figsize = (6,4))
     plt.plot(trange, scores)
     plt.xticks(trange)
     plt.xlabel('Number of Topics')
     plt.ylabel('coherence score')
-    plt.savefig(os.path.join(OUT_DIR, 'coherence_Topics_plot.png'))
+    plt.savefig(os.path.join(OUT_DIR, f'coherence_Topics_plot_{method}.png'))
     plt.show()
 
     
 
 ##Some functions for Topic analysis##
-def format_topics_sentences(ldamodel, corpus, texts):
+def format_topics_sentences(model, corpus, texts):
     # Init output
     sent_topics_df = pd.DataFrame()
 
     # Get main topic in each document
-    for i, row_list in enumerate(ldamodel[corpus]):
-        row = row_list[0] if ldamodel.per_word_topics else row_list
+    for i, row_list in enumerate(model[corpus]):
+        row = row_list[0] if model.per_word_topics else row_list
         # print(row)
         row = sorted(row, key=lambda x: (x[1]), reverse=True)
         # Get the Dominant topic, Perc Contribution and Keywords for each document
         for j, (topic_num, prop_topic) in enumerate(row):
             if j == 0:  # => dominant topic
-                wp = ldamodel.show_topic(topic_num)
+                wp = model.show_topic(topic_num)
                 topic_keywords = ", ".join([word for word, prop in wp])
                 sent_topics_df = sent_topics_df.append(
                     pd.Series([int(topic_num), round(prop_topic, 4), topic_keywords]), ignore_index=True)
@@ -102,7 +128,7 @@ def format_topics_sentences(ldamodel, corpus, texts):
 
 
 # Sentence Coloring of N Sentences
-def sentences_chart(ldamodel, corpus, start=0, end=12):
+def sentences_chart(model, corpus, start=0, end=12):
     corp = corpus[start:end]
     mycolors = [color for name, color in mcolors.XKCD_COLORS.items()]
 
@@ -111,8 +137,8 @@ def sentences_chart(ldamodel, corpus, start=0, end=12):
     for i, ax in enumerate(axes):
         if i > 0:
             corp_cur = corp[i - 1]
-            topic_percs, wordid_topics, wordid_phivalues = ldamodel[corp_cur]
-            word_dominanttopic = [(ldamodel.id2word[wd], topic[0]) for wd, topic in wordid_topics]
+            topic_percs, wordid_topics, wordid_phivalues = model[corp_cur]
+            word_dominanttopic = [(model.id2word[wd], topic[0]) for wd, topic in wordid_topics]
             ax.text(0.01, 0.5, "Doc " + str(i - 1) + ": ", verticalalignment='center',
                     fontsize=16, color='black', transform=ax.transAxes, fontweight=700)
 
@@ -145,12 +171,12 @@ def sentences_chart(ldamodel, corpus, start=0, end=12):
 
 
     
-def topics_per_document(ldamodel, corpus, start=0, end=-1):
+def topics_per_document(model, corpus, start=0, end=-1):
     corpus_sel = corpus[start:end]
     dominant_topics = []
     topic_percentages = []
     for i, corp in enumerate(corpus_sel):
-        topic_percs, wordid_topics, wordid_phivalues = ldamodel[corp]
+        topic_percs, wordid_topics, wordid_phivalues = model[corp]
         dominant_topic = sorted(topic_percs, key=lambda x: x[1], reverse=True)[0][0]
         dominant_topics.append((i, dominant_topic))
         topic_percentages.append(topic_percs)
@@ -169,15 +195,15 @@ def topics_per_document(ldamodel, corpus, start=0, end=-1):
 
 
 # Plot Topic Distribution by Dominant Topics
-def Plot_topic_dist(ldamodel, num_topics, corpus):
-    topic_top3words = [(i, topic) for i, topics in ldamodel.show_topics(num_topics, num_words=20, formatted=False)
+def Plot_topic_dist(model, num_topics, corpus):
+    topic_top3words = [(i, topic) for i, topics in model.show_topics(num_topics, num_words=20, formatted=False)
                        for j, (topic, wt) in enumerate(topics) if j < 3]
 
     df_top3words_stacked = pd.DataFrame(topic_top3words, columns=['topic_id', 'words'])
     df_top3words = df_top3words_stacked.groupby('topic_id').agg(', \n'.join)
     df_top3words.reset_index(level=0, inplace=True)
 
-    df_dominant_topic_in_each_doc, df_topic_weightage_by_doc = topics_per_document(ldamodel, corpus, start=0, end=-1)
+    df_dominant_topic_in_each_doc, df_topic_weightage_by_doc = topics_per_document(model, corpus, start=0, end=-1)
 
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6), dpi=100, sharey=True)
     plt.subplots_adjust(hspace=0.7)
@@ -204,8 +230,8 @@ def Plot_topic_dist(ldamodel, num_topics, corpus):
 
 # Word Counts of Topic Keywords
 #TODO change this function to be general - now works for 9 topics and y_lim is proper of one question
-def word_count_topic(ldamodel, num_topics, texts):
-    topics = ldamodel.show_topics(num_topics=num_topics, formatted=False)
+def word_count_topic(model, num_topics, texts):
+    topics = model.show_topics(num_topics=num_topics, formatted=False)
     data_flat = [w for w_list in texts for w in w_list]
     counter = Counter(data_flat)
 
@@ -241,14 +267,14 @@ def word_count_topic(ldamodel, num_topics, texts):
 
     
 # Wordcloud
-def get_wordcloud_LDA(ldamodel, num_topics):
+def get_wordcloud(method , model, num_topics):
 #     cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
 
     cloud = WordCloud(width=800, height=560,
                       background_color='white', collocations=False,
                       min_font_size=10)
 
-    topics = ldamodel.show_topics(num_topics=num_topics, num_words=50, formatted=False)
+    topics = model.show_topics(num_topics=num_topics, num_words=50, formatted=False)
 
     for i in range(num_topics):
         topic_words = dict(topics[i][1])
@@ -261,7 +287,7 @@ def get_wordcloud_LDA(ldamodel, num_topics):
         plt.axis('off')
         plt.margins(x=0, y=0)
         plt.tight_layout(pad=0)
-        dr = '{}/wordCloud/LDA'.format(OUT_DIR)
+        dr = f'{OUT_DIR}/wordCloud/{method}'
         if not os.path.exists(dr):
             os.makedirs(dr)
         plt.savefig(os.path.join(dr, f'Topic{i}__wordcloud.png'))
@@ -273,11 +299,11 @@ def get_wordcloud_LDA(ldamodel, num_topics):
 #########################
 
 # t-SNE Clustering Chart
-def tsne_plot(ldamodel, corpus, num_topics,
+def tsne_plot(model, corpus, num_topics,
               Keep_well_separated_pcnt=0.2):  # Keep_well_separated_pcnt = 0: keep all points
     # Get topic weights
     topic_weights = []
-    for i, row_list in enumerate(ldamodel[corpus]):
+    for i, row_list in enumerate(model[corpus]):
         topic_weights.append([w for i, w in row_list[0]])
 
     # Array of topic weights    
@@ -307,18 +333,18 @@ def tsne_plot(ldamodel, corpus, num_topics,
 
 
 # pyLDAVis
-def pyldavis_plot(ldamodel, corpus, mds="tsne"):
+def pyldavis_plot(model, corpus, mds="tsne"):
     pyLDAvis.enable_notebook()
-    vis = pyLDAvis.gensim.prepare(ldamodel, corpus, dictionary=ldamodel.id2word, mds=mds)
+    vis = pyLDAvis.gensim.prepare(model, corpus, dictionary=model.id2word, mds=mds)
     return vis
 
 
 # Top 3 topics per document
-def top3_topics_per_document(ldamodel, corpus):
+def top3_topics_per_document(model, corpus):
     topics_top3 = []
 
     for i, corp in enumerate(corpus):
-        topic_percs, wordid_topics, wordid_phivalues = ldamodel[corp]
+        topic_percs, wordid_topics, wordid_phivalues = model[corp]
         dominant_topic = sorted(topic_percs, key=lambda x: x[1], reverse=True)[0][0]
         try:
             topic2 = sorted(topic_percs, key=lambda x: x[1], reverse=True)[1][0]
